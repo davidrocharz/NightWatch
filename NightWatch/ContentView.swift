86c38c21c287d943f5807f6ea59f51a18a7ae620
David Rocharz <davidrocharz@gmail.com>
2024-09-22 13:40:12 -0400

    Implement (static) List / Section views

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -7,73 +7,53 @@
 
 import SwiftUI
 
+var nightlyTasks = [
+    "Check all windows",
+    "Check all doors",
+    "Check that the safe is locked",
+    "Check that the mailbox is empty",
+    "Check security cameras",
+    "Clear ice from the sidewalks",
+    "Document \"strange and unusual occurrences\""
+]
+
+var weeklyTasks = [
+    "Check all windows",
+    "Check all doors",
+    "Check that the safe is locked",
+    "Check that the mailbox is empty"
+]
+
+var monthlyTasks = [
+    "Check all windows",
+    "Check all doors"
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    // MARK: Nightly Tasks
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "moon.stars"))
-                                .font(.title)
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
-                                .padding(.top)
-                            Text("Nightly Tasks")
-                                .font(.title)
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
-                                .textCase(.uppercase)
-                                .padding(.top)
-                        }
-                        Text("- Check all windows")
-                        Text("- Check all doors")
-                        Text("- Check that the safe is locked")
-                        Text("- Check that the mailbox is empty")
-                        Text("- Check security cameras")
-                        Text("- Clear ice from sidewalks")
-                        Text("- Document \"strange and unusual occurances\"")
-                    }
-                    Divider()
-                    // MARK: Weekly Tasks
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "sunset"))
-                                .font(.title)
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
-                                .padding(.top)
-                            Text("Weekly Tasks")
-                                .font(.title)
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
-                                .textCase(.uppercase)
-                                .padding(.top)
-                        }
-                    }
-                    Divider()
-                    // MARK: Monthly Tasks
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "calendar"))
-                                .font(.title)
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
-                                .padding(.top)
-                            Text("Monthly Tasks")
-                                .font(.title)
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
-                                .textCase(.uppercase)
-                                .padding(.top)
-                        }
-                    }
-                }.padding([.top, .leading])
-                Spacer()
+        List {
+            Section(header: HStack(alignment: .bottom) {Image(systemName: "moon.stars")
+                Text("Nightly Tasks")
+            }.font(.title).foregroundColor(.yellow).fontWeight(.heavy)) {
+                ForEach(nightlyTasks, id: \.self) { task in
+                    Text(task)
+                }
+            }
+            Section(header: HStack(alignment: .bottom) {Image(systemName: "sunset")
+                Text("Weekly Tasks")
+            }.font(.title).foregroundColor(.yellow).fontWeight(.heavy)) {
+                ForEach(weeklyTasks, id: \.self) { task in
+                    Text(task)
+                }
+            }
+            Section(header: HStack(alignment: .bottom) {Image(systemName: "calendar")
+                Text("Monthly Tasks")
+            }.font(.title).foregroundColor(.yellow).fontWeight(.heavy)) {
+                ForEach(monthlyTasks, id: \.self) { task in
+                    Text(task)
+                }
             }
         }
-        Spacer()
     }
 }
 
