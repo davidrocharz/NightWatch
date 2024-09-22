1f7d81eddad52034a10abe19cf637c705a86f08c
David Rocharz <davidrocharz@gmail.com>
2024-09-21 23:30:56 -0400

    Initial Commit

---
+++ NightWatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NightWatch
+//
+//  Created by David Rocharz on 9/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by David Rocharz on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
