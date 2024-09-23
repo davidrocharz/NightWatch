4cc174a3e3d919066aff1a0b9007b666aa6f0eac
David Rocharz <davidrocharz@gmail.com>
2024-09-23 13:42:05 -0400

    Size class support in DetailsView

--- NightWatch/DetailsView.swift
+++ NightWatch/DetailsView.swift
@@ -8,15 +8,25 @@ import SwiftUI
 
 struct DetailsView: View {
     @Binding var task: Task
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     
     var body: some View {
         VStack {
+            HStack {
+                Image(systemName: "map")
+                Text("Placeholder for hotel floor plan")
+            }
             Text(task.name)
-            Text("Placeholder description")
+            
+            if verticalSizeClass == .regular {
+                Divider()
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed velit sit amet massa scelerisque imperdiet. Etiam tempor ex a lectus bibendum aliquam. Nulla bibendum ac nunc ullamcorper ultricies. Curabitur tristique tincidunt metus, in lacinia felis lobortis ac. Duis ut leo tortor. Sed efficitur ex id ipsum porttitor, eget hendrerit lectus porta. Nam eget congue lectus, sit amet dapibus lectus.")
+            }
+            Divider()
             Button("Mark Complete") {
                 task.isCompleted = true
             }
-        }
+        }.padding(.horizontal)
     }
 }
 
